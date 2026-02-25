68604eb41d589fa822c2bc8ad6b4a3954c6dc0ee
valoch29 <73793233+valoch29@users.noreply.github.com>
2026-02-25 09:40:57 +0000

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,26 +1,41 @@
 # dictionary.py
-CORRECTIONS = {
-    # Chiffres et Phonétique NATO
+replacement_dict = {
+    # Erreurs d'identification Aéroport/Service
+    "STARWING AIRPORT": "TALLINN AIRPORT",
+    "THIS IS STARWING": "THIS IS TALLINN",
+    "BUSINESS TRAVELLING": "THIS IS TALLINN",
+    
+    # Phonétique & Alphabet
+    "UNICORN": "UNIFORM",
     "NINER": "9", "ZE-RO": "0", "WUN": "1", "TOO": "2", "TREE": "3", 
     "FOW-ER": "4", "FIFE": "5", "SIX": "6", "SEV-EN": "7", "AIT": "8",
     "ZERO": "0", "ONE": "1", "TWO": "2", "THREE": "3", "FOUR": "4", 
     "FIVE": "5", "SEVEN": "7", "EIGHT": "8", "NINE": "9",
     
+    # Piste & État (Spécifique Tallinn)
+    "PACHYDOWN": "TOUCHDOWN",
+    "PACK DOWN": "TOUCHDOWN",
+    "TOUCH STONE": "TOUCHDOWN",
+    "MEAT POINT": "MIDPOINT",
+    "MADE POINT": "MIDPOINT",
+    "STOP END": "STOP END",
+    "DEPOSIT": "DEPOSITS",
+    "RUN THE CONDITION": "RUNWAY CONDITION",
+    
     # Météo & Vent
-    "THRUST": "FROST", "TRUST": "FROST", "FORST": "FROST", "FRONT": "FROST",
-    "NOS": "KNOTS", "NOTS": "KNOTS", "NOT": "KNOTS", "KNOT": "KNOTS",
-    "CABLE K": "CAVOK", "CABO K": "CAVOK", "COVER K": "CAVOK", "CAV OK": "CAVOK",
-    "DUE POINT": "DEWPOINT", "DO POINT": "DEWPOINT", "DEW POINT": "DEWPOINT",
+    "THRUST": "FROST", "TRUST": "FROST", "FORST": "FROST",
+    "NOS": "KNOTS", "NOTS": "KNOTS", "KNOT": "KNOTS",
+    "CABLE K": "CAVOK", "CABO K": "CAVOK", "CAV OK": "CAVOK",
+    "DUE POINT": "DEWPOINT", "DO POINT": "DEWPOINT",
     "NO SEEK": "NOSIG", "NO SICK": "NOSIG", "NO SIGN": "NOSIG",
+    "NO FIT": "NOSIG",
+    "KVK": "CAVOK",
     
-    # Piste & État
-    "PACK DOWN": "TOUCHDOWN", "TOUCH STONE": "TOUCHDOWN", "TOUCH DOWN": "TOUCHDOWN",
-    "MEAT POINT": "MIDPOINT", "MID POINT": "MIDPOINT", "MADE POINT": "MIDPOINT",
-    "TOP END": "STOP END", "STOP-END": "STOP END", "STOPEND": "STOP END",
-    "WEST": "WET", "WASTE": "WET", "WEIGHT": "WET", "WAIT": "WET",
+    # État de surface
+    "WET FINISH": "VICINITY",
+    "WEST": "WET", "WASTE": "WET", "WEIGHT": "WET",
     
-    # Lieux & Général
-    "BUSINESS TRAVELLING": "THIS IS TALLINN",
+    # Divers
     "PHOENIX HILL": "VICINITY OF THE",
     "HECTOPASCALSS": "HECTOPASCALS", "HECTO PASCALS": "HECTOPASCALS"
 }
