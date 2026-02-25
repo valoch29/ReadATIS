a6fd962567ed3311250c1b7ce7dd9e2295f059ad
valoch29 <73793233+valoch29@users.noreply.github.com>
2026-02-25 06:42:57 +0200

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,26 @@
+# dictionary.py
+CORRECTIONS = {
+    # Chiffres et Phonétique NATO
+    "NINER": "9", "ZE-RO": "0", "WUN": "1", "TOO": "2", "TREE": "3", 
+    "FOW-ER": "4", "FIFE": "5", "SIX": "6", "SEV-EN": "7", "AIT": "8",
+    "ZERO": "0", "ONE": "1", "TWO": "2", "THREE": "3", "FOUR": "4", 
+    "FIVE": "5", "SEVEN": "7", "EIGHT": "8", "NINE": "9",
+    
+    # Météo & Vent
+    "THRUST": "FROST", "TRUST": "FROST", "FORST": "FROST", "FRONT": "FROST",
+    "NOS": "KNOTS", "NOTS": "KNOTS", "NOT": "KNOTS", "KNOT": "KNOTS",
+    "CABLE K": "CAVOK", "CABO K": "CAVOK", "COVER K": "CAVOK", "CAV OK": "CAVOK",
+    "DUE POINT": "DEWPOINT", "DO POINT": "DEWPOINT", "DEW POINT": "DEWPOINT",
+    "NO SEEK": "NOSIG", "NO SICK": "NOSIG", "NO SIGN": "NOSIG",
+    
+    # Piste & État
+    "PACK DOWN": "TOUCHDOWN", "TOUCH STONE": "TOUCHDOWN", "TOUCH DOWN": "TOUCHDOWN",
+    "MEAT POINT": "MIDPOINT", "MID POINT": "MIDPOINT", "MADE POINT": "MIDPOINT",
+    "TOP END": "STOP END", "STOP-END": "STOP END", "STOPEND": "STOP END",
+    "WEST": "WET", "WASTE": "WET", "WEIGHT": "WET", "WAIT": "WET",
+    
+    # Lieux & Général
+    "BUSINESS TRAVELLING": "THIS IS TALLINN",
+    "PHOENIX HILL": "VICINITY OF THE",
+    "HECTOPASCALSS": "HECTOPASCALS", "HECTO PASCALS": "HECTOPASCALS"
+}
