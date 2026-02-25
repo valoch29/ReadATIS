648ba606163ec766f3501889708a72a56fdc5d43
valoch29 <73793233+valoch29@users.noreply.github.com>
2026-02-25 18:39:41 +0200

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,38 +1,64 @@
 replacement_dict = {
-    # --- Termes Clés (Ancre de Regex) ---
+    # --- Identification Aéroport & Aéroport ---
     "INFLATION": "INFORMATION",
     "INFORMAATION": "INFORMATION",
-    "DEW POINT": "DEWPOINT",
+    "DARWIN AIRPORT": "TALLINN AIRPORT",
+    "DARLING AIRPORT": "TALLINN AIRPORT",
+    "BUSINESS TALENT": "THIS IS TALLINN",
+    "STARLIN": "TALLINN",
+    "TELLING": "TALLINN",
+    "STALIN": "TALLINN",
+    "TALIN": "TALLINN",
+
+    # --- Piste & Zones RCC (Crucial) ---
+    "PATCH DOWN": "TOUCHDOWN",
+    "PACHYDOWN": "TOUCHDOWN",
+    "TOUCH DOWN": "TOUCHDOWN",
+    "2UCHDOWN": "TOUCHDOWN",
+    "MEET POINT": "MIDPOINT",
+    "MEAT POINT": "MIDPOINT",
+    "MADE POINT": "MIDPOINT",
+    "MID POINT": "MIDPOINT",
+    "2P END": "STOP END",
+    "SSTOP END": "STOP END",
+    "STOPEND": "STOP END",
+    "TOP END": "STOP END",
+    "STUPEND": "STOP END",
+    "DEPOSE IT": "DEPOSITS",
+    "BEFORE IT": "DEPOSITS",
+    "DEPOSIT": "DEPOSITS",
+
+    # --- Météo & Visibilité ---
+    "CAPITAL K": "CAVOK",
+    "CAV OK": "CAVOK",
+    "CABLE K": "CAVOK",
+    "KVK": "CAVOK",
+    "CARGO OKAY": "CAVOK",
+    "CABO K": "CAVOK",
+    "CABO OK": "CAVOK",
+    "NO SEAT": "NOSIG",
+    "NO SEEK": "NOSIG",
+    "NO SICK": "NOSIG",
+    "NO THICK": "NOSIG",
+    "NO SIGN": "NOSIG",
+    "NO FIT": "NOSIG",
+    "NO 6": "NOSIG",
+    "NO SIX": "NOSIG",
+    "THRUST": "FROST",
+    "TRUST": "FROST",
     "VIEW POINT": "DEWPOINT",
     "VIEWPOINT": "DEWPOINT",
     "DUE POINT": "DEWPOINT",
-    
-    # --- Localisation ---
-    "DARWIN AIRPORT": "TALLINN AIRPORT",
-    "TALIN": "TALLINN",
-    "STARLIN": "TALLINN",
-    "BUSINESS TALENT": "TALLINN",
-    
-    # --- Météo & États ---
-    "CABO K": "CAVOK",
-    "CABO OK": "CAVOK",
-    "CAV OK": "CAVOK",
-    "NO SEEK": "NOSIG",
-    "NO SICK": "NOSIG",
-    "NOSIG": "NOSIG",
-    
-    # --- Chiffres (Whisper adore écrire en lettres) ---
+
+    # --- Chiffres & Unités ---
     "ZERO": "0", "ONE": "1", "TWO": "2", "THREE": "3", "FOUR": "4",
     "FIVE": "5", "FIFE": "5", "SIX": "6", "SEVEN": "7", "EIGHT": "8",
-    "NINE": "9", "NINER": "9", "TREE": "3", "TO": "2",
-    
-    # --- Unités ---
-    "KKNOTS": "KNOTS",
-    "KNOTS": "KNOTS",
-    "HECTOPASCAL": "HPA",
-    "HECTOPASCALS": "HPA",
-    
-    # --- Alphabet ---
-    "GULF": "G", "HOTEL": "H", "INDIA": "I", "JULIET": "J", "KILO": "K", 
-    "LIMA": "L", "MIKE": "M", "NOVEMBER": "N", "OSCAR": "O", "PAPA": "P"
+    "NINE": "9", "NINER": "9", "TREE": "3", "TO": "2", "AIT": "8",
+    "KKNOTSS": "KNOTS", "KKNOTS": "KNOTS", "NOTS": "KNOTS",
+    "HECTO PASCAL": "HPA", "HECTO PASCALS": "HPA", "HECTOPASCAL": "HPA",
+
+    # --- Alphabet Radio ---
+    "MIC": "MIKE", "MIKE": "M", "GULF": "G", "HOTEL": "H", "INDIA": "I", 
+    "JULIET": "J", "KILO": "K", "LIMA": "L", "NOVEMBER": "N", "OSCAR": "O",
+    "PAPA": "P", "QUEBEC": "Q", "ROMEO": "R", "SIERRA": "S", "TANGO": "T"
 }
