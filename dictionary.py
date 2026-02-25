f529993fa7a68f91a1921ec6e612677b70408387
valoch29 <73793233+valoch29@users.noreply.github.com>
2026-02-25 11:31:49 +0000

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,41 +1,36 @@
 # dictionary.py
 replacement_dict = {
-    # Erreurs d'identification Aéroport/Service
+    # Identification de l'aéroport
+    "TELLING AIRPORT": "TALLINN AIRPORT",
+    "THIS IS TELLING": "THIS IS TALLINN",
     "STARWING AIRPORT": "TALLINN AIRPORT",
     "THIS IS STARWING": "THIS IS TALLINN",
-    "BUSINESS TRAVELLING": "THIS IS TALLINN",
     
-    # Phonétique & Alphabet
-    "UNICORN": "UNIFORM",
-    "NINER": "9", "ZE-RO": "0", "WUN": "1", "TOO": "2", "TREE": "3", 
-    "FOW-ER": "4", "FIFE": "5", "SIX": "6", "SEV-EN": "7", "AIT": "8",
+    # Phonétique & Chiffres
+    "9ER": "9",
+    "KKNOTSS": "KNOTS",
+    "KKNOTS": "KNOTS",
     "ZERO": "0", "ONE": "1", "TWO": "2", "THREE": "3", "FOUR": "4", 
-    "FIVE": "5", "SEVEN": "7", "EIGHT": "8", "NINE": "9",
+    "FIVE": "5", "SIX": "6", "SEVEN": "7", "EIGHT": "8", "NINE": "9",
     
-    # Piste & État (Spécifique Tallinn)
-    "PACHYDOWN": "TOUCHDOWN",
-    "PACK DOWN": "TOUCHDOWN",
-    "TOUCH STONE": "TOUCHDOWN",
-    "MEAT POINT": "MIDPOINT",
-    "MADE POINT": "MIDPOINT",
-    "STOP END": "STOP END",
-    "DEPOSIT": "DEPOSITS",
-    "RUN THE CONDITION": "RUNWAY CONDITION",
-    
-    # Météo & Vent
-    "THRUST": "FROST", "TRUST": "FROST", "FORST": "FROST",
-    "NOS": "KNOTS", "NOTS": "KNOTS", "KNOT": "KNOTS",
-    "CABLE K": "CAVOK", "CABO K": "CAVOK", "CAV OK": "CAVOK",
-    "DUE POINT": "DEWPOINT", "DO POINT": "DEWPOINT",
-    "NO SEEK": "NOSIG", "NO SICK": "NOSIG", "NO SIGN": "NOSIG",
+    # Météo & Visibilité
+    "CARGO OKAY": "CAVOK",
+    "NO THICK": "NOSIG",
     "NO FIT": "NOSIG",
     "KVK": "CAVOK",
+    "CABLE K": "CAVOK",
     
-    # État de surface
+    # Piste & Zones (Spécifique Tallinn)
+    "PACHYDOWN": "TOUCHDOWN",
+    "TOUCH DOWN Z1, 0": "TOUCHDOWN ZONE",
+    "Z1, 0": "ZONE",
+    "TOP END": "STOP END",
+    "DEPOSIT": "DEPOSITS",
+    "RUN THE CONDITION": "RUNWAY CONDITION",
+    "WEST": "WET",
     "WET FINISH": "VICINITY",
-    "WEST": "WET", "WASTE": "WET", "WEIGHT": "WET",
     
     # Divers
-    "PHOENIX HILL": "VICINITY OF THE",
-    "HECTOPASCALSS": "HECTOPASCALS", "HECTO PASCALS": "HECTOPASCALS"
+    "HECTOPASCALSS": "HECTOPASCALS",
+    "HECTO PASCALS": "HECTOPASCALS"
 }
