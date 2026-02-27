1ab22f217cb656c76eaa08a00ebd2b30a777fc9e
ATISreader <73793233+ATISreader@users.noreply.github.com>
2026-02-27 10:41:41 +0000

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -10,7 +10,7 @@ replacement_dict = {
     "STALIN": "TALLINN",
     "TALIN": "TALLINN",
 
-    # --- Piste & RCC (Codes de condition) ---
+    # --- Piste & RCC ---
     "PATCH DOWN": "TOUCHDOWN",
     "PACHYDOWN": "TOUCHDOWN",
     "TOUCH DOWN": "TOUCHDOWN",
@@ -19,40 +19,35 @@ replacement_dict = {
     "MEAT POINT": "MIDPOINT",
     "MADE POINT": "MIDPOINT",
     "MID POINT": "MIDPOINT",
-    "TOP-END": "STOP END",
-    "CROSS-END": "STOP END",
-    "2P END": "STOP END",
-    "SSTOP END": "STOP END",
     "STOPEND": "STOP END",
-    "TOP END": "STOP END",
-    "STUPEND": "STOP END",
     "STOP-END": "STOP END",
-    "52P END": "5 STOP END",
+    "TOP END": "STOP END",
+    "TOP-END": "STOP END",
+    "STUPEND": "STOP END",
     "IN YOU": "IN USE",
     "IN YOUTH": "IN USE",
-    
+    "PATRICK IN USE": "RUNWAY 26 IN USE", # Correction pour "Patrick"
 
     # --- Météo & Tendance ---
     "CAPITAL K": "CAVOK",
     "CAV OK": "CAVOK",
     "CABLE K": "CAVOK",
     "CABO K": "CAVOK",
-    "CABO OK": "CAVOK",
     "NO SEAT": "NOSIG",
     "NO SEEK": "NOSIG",
     "NO SICK": "NOSIG",
-    "NO 6": "NOSIG",
-    "NO SIX": "NOSIG",
     "NO SIGN": "NOSIG",
     "VIEW POINT": "DEWPOINT",
     "VIEWPOINT": "DEWPOINT",
     "DUE POINT": "DEWPOINT",
-    "EXTRA PASCAL": "HECTOPASCAL",
+    "EXTRA PASCAL": "HPA",
+    "ESTHER PASCAL": "HPA", # Correction pour "Esther"
+    "HECTOPASCAL": "HPA",
 
     # --- Chiffres & Alphabet ---
     "ZERO": "0", "ONE": "1", "TWO": "2", "THREE": "3", "FOUR": "4",
     "FIVE": "5", "FIFE": "5", "SIX": "6", "SEVEN": "7", "EIGHT": "8",
-    "NINE": "9", "NINER": "9", "TREE": "3", "AIT": "8",
-    "MIC": "MIKE", "MIKE": "M", "GULF": "G", "HOTEL": "H", "INDIA": "I", "JULIET": "J",
-    "KKNOTS": "KNOTS", "NOTS": "KNOTS", "HPA": "HPA", "HECTOPASCAL": "HPA"
+    "NINE": "9", "NINER": "9", "TREE": "3", "AIT": "8", "6-5": "65",
+    "MIKE": "M", "GULF": "G", "HOTEL": "H", "INDIA": "I", "JULIET": "J", "VICTOR": "V",
+    "KKNOTS": "KNOTS", "NOTS": "KNOTS"
 }
